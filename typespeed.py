3b93def783dc3d960e56faaf384c61433c3ab752
Gabe <gab.01@hotmail.com>
2019-10-20 19:26:20 -0400

    Add files via upload

--- typespeed.py
+++ typespeed.py
@@ -1,6 +1,7 @@
-import sys, time
+import sys
+import matplotlib.pyplot as plt
+import time
 from random import randint
-
 
 #Variables
 prompt = ""
@@ -8,17 +9,6 @@ wordslist = []
 wpm = timed = 0
 response = ""
 
-def main():
-    global wordslist, wpm, timed, response, prompt
-    
-
-    #Opening from a file of 10,000 words and putting all the words into a list
-    words = open("words.txt", "r")
-    wordslist = words.read()
-    wordslist = wordslist.split()
-    
-    #Run the program
-    test()
 
 def createPrompt(le, wordsl):
     ret = ""
@@ -29,11 +19,10 @@ def createPrompt(le, wordsl):
 def test():
     global wordslist, response, prompt, timed, wpm
     #Create the prompt and show it to the user
-    prompt = createPrompt(20, wordslist)
+    prompt = createPrompt(10, wordslist)
     print("\n",prompt,"\n", sep ="")
 
-
-    #Have the user type in the prompt 
+    #Have the user type in the prompt
     print("Type the above prompt in...")
 
     #Countdown to typing
@@ -42,6 +31,7 @@ def test():
         time.sleep(1)
 
     #Check how long it took to type and store that amount of time in the time variable
+    print("Start!")
     timed = time.time()
     response = input()
     timed = round(time.time() - timed, 2)
@@ -50,21 +40,63 @@ def test():
     #print("Time:",timed)
     #print("Characters:",len(response))
     wpm = int((len(response)/5)/(timed/60))
-    restart()
-    
+    main()
 
-def restart():
+def wpmavg():
+    #Read from a file of past WPM and add that to a table of values
+    wpmfile = open("wpmhistory.txt", "r")
+    #Turn the file speeds into a list
+    wpms = [int(i) for i in wpmfile.read().split()]
+    #Print out the average wpm
+    print("\n\nYour average typing speed is",sum(wpms)/len(wpms),"Words Per Minute!")
+    wpmfile.close()
+
+def wpmadd(num):
+    #Adds given wpm to the text file of words per minute
+    infile = open("wpmhistory.txt","a")
+    print(str(num) + " ", file=infile)
+    infile.close()
+
+def wpmgraph():
+    file = open("wpmhistory.txt", "r")
+    speeds = [int(i) for i in file.read().split()]
+    ys = [i for i in range(len(speeds))]
+    fig, graph = plt.subplots()
+    graph.plot(ys,speeds)
+    plt.show()
+    file.close()
+
+def main():
     global wordslist, response, prompt, timed, wpm
+    #Opening from a file of 10,000 words and putting all the words into a list
+    words = open("words.txt", "r")
+    wordslist = words.read()
+    wordslist = wordslist.split()
+
     #Check if they wrote the prompt and print out wpm with a cute statement
-    if(response == prompt):
+    if prompt == "":
+        print()
+    elif(response == prompt):
         print("Great job, you typed at", wpm,"Words Per Minute!")
+        wpmadd(wpm)
+    elif response == "avg":
+        print()
+    elif response == "graph":
+        print()
     else:
         print("You made a typing error, however you typed at",wpm,"Words Per Minute!")
 
-    response = input("Would you like to go again? (y/n)")
-    if(response == "y"):
-        #TODO: Make the whole program run again
+    response = input("Would you like to race?\n\t'Y' for yes\n\t'N' for no\n\t'AVG' for your wpm avg\n\t'GRAPH' for graph of WPM history\n\t")
+    if(response.lower().strip() == "y"):
+        #restarts the program
         test()
+    elif(response.lower().strip() == "avg"):
+        #Displays their wpm average
+        wpmavg()
+        main()
+    elif(response.lower().strip() == "graph"):
+        wpmgraph()
+        main()
     else:
         sys.exit()
 
